68f9b6835099fe53eba31e5cfba56ba1a85f874e
LynxieBoo <rysitsa@yandex.ru>
2019-09-04 10:32:32 +0300

    Add delay and default language = 'en' to fixture 'browser'

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: en, ru, ... (ect.)")
 
 
@@ -28,5 +29,6 @@ def browser(request):
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
+    time.sleep(5)
     print("\nQuit browser..")
     browser.quit()
